d9ccd692cfa47cd546c351ac7d1343bdbb7e75fa
Jeniffer Leme <162703146+jeniffer-leme@users.noreply.github.com>
2026-06-21 20:15:29 -0300

    Create 01-Alunos.java

---
+++ 01-Filas/01-Alunos.java
@@ -0,0 +1,38 @@
+package exercicios.beluzo.ex01;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Alunos {
+    public static void main(String[] args) {
+        List<String> nomes = new ArrayList();
+        
+        nomes.add("Maria");
+        nomes.add("Alef");
+        nomes.add("Camilla");
+        nomes.add("Gustavo");
+        nomes.add("Victor");
+        
+        System.out.println("=====ALUNOS=====");
+        for( String nome : nomes) {
+            System.out.println(nome);
+        }
+        
+        System.out.println();
+        
+        System.out.println("Tamanho da lista: " + nomes.size());
+        
+        System.out.println("--------------------------------");
+        
+        nomes.remove("Gustavo");
+        
+        System.out.println("=====ALUNOS=====");
+        for(String nome : nomes) {
+            System.out.println(nome);
+        }
+        
+        System.out.println();
+        
+        System.out.println("Tamanho da lista: " + nomes.size());
+    }
+}
