4e431ce2f59105099d6be5c68ba538fb80564c1f
Jeniffer Leme <162703146+jeniffer-leme@users.noreply.github.com>
2026-06-21 20:21:38 -0300

    Add Numeros class to display list elements

---
+++ 01-Filas/02-Numeros.java
@@ -0,0 +1,24 @@
+package exercicios.beluzo.ex02;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Numeros {
+    public static void main(String[] args) {
+        List<Integer> numero = new ArrayList();
+        
+        numero.add(12);
+        numero.add(3);
+        numero.add(89);
+        numero.add(21);
+        numero.add(0);
+        numero.add(29);
+        numero.add(45);
+        numero.add(9);
+        numero.add(52);
+        numero.add(90);
+        
+        System.out.println("1° Elemento: " + numero.get(0));
+        System.out.println("5° Elemento: " + numero.get(4));
+    }
+}
