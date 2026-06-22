4ffd852108aa9cfafb8cfce9520fc4e84e581ffc
Jeniffer Leme <162703146+jeniffer-leme@users.noreply.github.com>
2026-06-21 21:16:35 -0300

    Create Main.java

---
+++ 01-Filas/07-Estudantes/Main.java
@@ -0,0 +1,25 @@
+package exercicios.beluzo.ex07;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        List<Aluno> aluno = new ArrayList();
+        
+        Aluno a1 = new Aluno("Romeu", 21, "Licenciatura");
+        Aluno a2 = new Aluno("Bruna", 19, "Administração");
+        Aluno a3 = new Aluno("Manuel", 25, "ADS");
+        
+        aluno.add(a1);
+        aluno.add(a2);
+        aluno.add(a3);
+        
+        System.out.println("====ALUNOS====");
+        for(Aluno a : aluno) {
+            System.out.println(a);
+            System.out.println("----------------------");
+        }
+    }
+}
