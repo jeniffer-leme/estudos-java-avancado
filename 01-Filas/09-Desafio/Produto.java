f77a4a0bc58d4bdb6d98624fc160d0434088bd80
Jeniffer Leme <162703146+jeniffer-leme@users.noreply.github.com>
2026-06-21 21:39:57 -0300

    Create Produto class with getters and setters

---
+++ 01-Filas/09-Desafio/Produto.java
@@ -0,0 +1,36 @@
+public class Produto {
+    private String nome;
+    private double preco;
+    private int quantidade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    @Override
+    public String toString() {
+        return "Nome: " + getNome() + "\n" +
+                "Preço: " + getPreco() + "\n" +
+                "Quantidade: " + getQuantidade();
+    }
+}
