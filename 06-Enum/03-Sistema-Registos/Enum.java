0b9d8a315217150c045f3b9ab2a462f223ec2794
Jeniffer Leme <162703146+jeniffer-leme@users.noreply.github.com>
2026-06-23 19:50:55 -0300

    Add enums for Prioridade, Status_Chamado, and Tipo_Problema

---
+++ 06-Enum/03-Sistema-Registos/Enum.java
@@ -0,0 +1,21 @@
+public class Enum {
+    public enum Prioridade {
+        BAIXA, 
+        MEDIA,
+        ALTA
+    }
+    
+    public enum Status_Chamado {
+        ABERTO,
+        EM_ATENDIMENTO,
+        RESOLVIDO,
+        CANCELADO
+    }
+    
+    public enum Tipo_Problema {
+        HARDWARE,
+        SOFTWARE,
+        REDE,
+        ACESSO
+    }
+}
