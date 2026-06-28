3438d050b64eeae5b5b3dfb507f157eafe1f93ba
Jeniffer Leme <162703146+jeniffer-leme@users.noreply.github.com>
2026-06-28 10:32:36 -0300

    Create Produtos class with getters and setters

---
+++ 10-JDBC/01-Banco-Dados/Produtos.java
@@ -0,0 +1,29 @@
+public class Produtos {
+    private String nome;
+    private Double preco;
+    private Integer quantidade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantity;
+    }
+}
