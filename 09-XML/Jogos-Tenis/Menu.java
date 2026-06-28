2a73f02a3be89329020ad53527807b9bf7ea30ea
Jeniffer Leme <162703146+jeniffer-leme@users.noreply.github.com>
2026-06-28 10:26:11 -0300

    Create Menu.java

---
+++ 09-XML/Jogos-Tenis/Menu.java
@@ -0,0 +1,16 @@
+public class Menu {
+    public void exibirMenu() {
+        System.out.println("=========== TORNEIO DE TÊNIS ===========");
+        System.out.println("1 - Criar XML");
+        System.out.println("2 - Adicionar jogo");
+        System.out.println("3 - Listar jogos");
+        System.out.println("4 - Alterar placar");
+        System.out.println("5 - Alterar quadra");
+        System.out.println("6 - Remover jogo");
+        System.out.println("7 - Buscar jogo");
+        System.out.println("8 - Contar jogos");
+        System.out.println("0 - Sair");
+        System.out.println("========================================");
+        System.out.print("Digite uma opção: ");
+    }
+}
